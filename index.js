8289be730a69bde6f93a19ffde0287bd2769a849
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2025-04-21 00:53:40 +0100

    added POST to index.js

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.post("/api/connect", (req, res) => {
+  const name = req.body.name;
+  const message = req.body.message;
+  res.json({message: "Hello ${name},your message has been forwarded to the campus connect team."});
+});
+
 app.listen(3000, () => {
   console.log("Server running");
 });
