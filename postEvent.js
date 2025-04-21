4170413f056b730eb5e965bde21e2679bb51edf2
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2025-04-21 01:45:08 +0100

    updated POST method

---
+++ postEvent.js
@@ -0,0 +1,13 @@
+export default function postEvent(app) {
+  app.post("/api/connect", (req, res) => {
+    try {
+      const eventData = req.body;
+      console.log("Received event data:", eventData);
+      res.status(200).send("Event data received successfully.");
+    } catch (error) {
+      console.error("Error processing event data:", error);
+      res.status(500).send("An error occurred while processing event data.");
+    }
+  });
+  
+}
